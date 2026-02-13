1b211f812bfe27bd93307c4911fa26fa52ccafd8
Pushkar~ <pushkarpatil8262@gmail.com>
2026-02-13 14:12:45 +0530

    first react file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <p>Hello, World!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-title">My React App</h1>
+      <ul className="navbar-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
